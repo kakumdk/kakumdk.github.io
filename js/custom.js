93b4176ba5f5d69882a9890f6b95c3343a1744b2
heykarthikwithu <heykarthikwithu@gmail.com>
2020-06-08 13:45:36 +0530

    test

--- js/custom.js
+++ js/custom.js
@@ -683,7 +683,16 @@ function writeQuotes(data) {
 /******************************************************************************************************************/
 $(function() {
     $(".read-article").click(function () {
-        readCurrentArticle();
+        // readCurrentArticle();
+
+        var u = new SpeechSynthesisUtterance();
+        u.text = 'Hello World';
+        u.lang = 'en-US';
+        u.rate = 1.2;
+        // u.onend = function(event) { alert('Finished in ' + event.elapsedTime + ' seconds.'); }
+        speechSynthesis.speak(u);
+
+
     });
 });
 function readCurrentArticle() {
