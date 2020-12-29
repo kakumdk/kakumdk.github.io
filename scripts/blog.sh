89ffd400679b30b10770a95196f2b1463b13a75a
heykarthikwithu <heykarthikwithu@gmail.com>
2020-12-29 07:43:53 +0530

    article

--- scripts/blog.sh
+++ scripts/blog.sh
@@ -1213,7 +1213,7 @@ jq -s '[.[][]]' $blogscontent'/'*.json > $blogscontentmerged'/blogs.json'
 date=$(date +'%Y-%m-%d')
 echo '<?xml version="1.0" encoding="UTF-8"?>
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-          <url><loc>https://peoplesblog.co.in</loc><lastmod>'$date'</lastmod></url>
+          <url><loc>https://peoplesblog.co.in/</loc><lastmod>'$date'</lastmod></url>
           <url><loc>https://peoplesblog.co.in/about.html</loc><lastmod>'$date'</lastmod></url>
           <url><loc>https://peoplesblog.co.in/links.html</loc><lastmod>'$date'</lastmod></url>
           <url><loc>https://peoplesblog.co.in/privacy.html</loc><lastmod>'$date'</lastmod></url>
