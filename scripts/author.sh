a09c7ed7794116f839bd73bc4ea0afcc24b3111a
heykarthikwithu <heykarthikwithu@gmail.com>
2021-01-04 18:47:33 +0530

    author css

--- scripts/author.sh
+++ scripts/author.sh
@@ -418,7 +418,7 @@ do
                             <hr class="invis">
                             <div class="row">
 
-                                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12 author-image-wrapper">
+                                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12 text-center author-image-wrapper">
                                     <img src="'$banner400x400'" alt="" class="img-fluid rounded-circle">
                                 </div><!-- end col -->
                                 <div class="col-lg-10 col-md-10 col-sm-10 col-xs-12 author-content-wrapper">
