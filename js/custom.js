a3c87471d4fe44a8ce7ef8c9e54aece4af85efc1
heykarthikwithu <heykarthikwithu@gmail.com>
2021-01-29 11:55:45 +0530

    social

--- js/custom.js
+++ js/custom.js
@@ -536,7 +536,7 @@ function writeBlogsFooter(data) {
     return output;
 }
 function writeSocialShareLinks(region) {
-    var title = $(document).find("title").text();
+    var title = $(document).find("title").text().replaceAll("|", "-");
     var url = window.location.href;
     var hashtags = 'People&#039;sBlog'
     var linkedin = 'http://www.linkedin.com/shareArticle?mini=true&amp;url=' + url;
@@ -552,8 +552,8 @@ function writeSocialShareLinks(region) {
             '<li class=""><a title="Share now on Twitter" href="'+twitter+'" target="_blank" class="btn-floating btn-lg btn-tw" type="button" role="button"><i class="fa fa-twitter"></i></a></li>' +
             '<li class=""><a title="Share now on Facebook" href="'+facebook+'" target="_blank" class="btn-floating btn-lg btn-fb" type="button" role="button"><i class="fa fa-facebook"></i></a></li>' +
             '<li class="whatsapp-desktop"><a title="Share now on Whatsapp web" href="'+whatsapp+'" target="_blank" class="btn-floating btn-lg btn-whatsapp" type="button" role="button"><i class="fa fa-whatsapp"></i></a></li>' +
-            '<li class="whatsapp-mobile"><a href="'+whatsapp_phone+'" target="_blank" class="btn-floating btn-lg btn-whatsapp" type="button" role="button"><i class="fa fa-whatsapp"></i></a></li>' +
-            '<li class="whatsapp-mobile sticky"><a href="'+whatsapp_phone+'" target="_blank" class="btn-floating btn-lg btn-whatsapp" type="button" role="button"><i class="fa fa-whatsapp"></i></a></li>' +
+            '<li class="whatsapp-mobile"><a href="'+whatsapp_phone+'" class="btn-floating btn-lg btn-whatsapp" type="button" role="button"><i class="fa fa-whatsapp"></i></a></li>' +
+            '<li class="whatsapp-mobile sticky"><a href="'+whatsapp_phone+'" class="btn-floating btn-lg btn-whatsapp" type="button" role="button"><i class="fa fa-whatsapp"></i></a></li>' +
             // '<li><a title="Share now on E-mail" href="'+mail+'" target="_blank" class="btn-floating btn-lg btn-email" type="button" role="button"><i class="fa fa-envelope"></i></a></li>' +
             '<li class="whatsapp-desktop"><a title="Copy Link" href="'+url+'" class="btn-floating btn-lg social-copy" type="button" role="button"><i class="fa fa-link"></i></a></li>' +
             '<li class="whatsapp-mobile"><a title="'+title+'" href="'+url+'" class="btn-floating btn-lg social-web-share-api" type="button" role="button"><i class="fa fa-share-alt"></i></a></li>' +
