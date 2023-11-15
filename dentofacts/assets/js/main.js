7c6ad240cce6dbf79cc33c43a549dec4dfebb95c
karthikdksrijan <karthik.dk@srijan.net>
2023-11-15 10:44:25 +0530

    update

--- dentofacts/assets/js/main.js
+++ dentofacts/assets/js/main.js
@@ -180,3 +180,18 @@ jQuery(document).ready(function ($) {
 
 
 });
+
+$(function() {
+    hideShareOnDesktop();
+});
+function hideShareOnDesktop() {
+    setInterval(() => {
+        var pageWidth = window.innerWidth;
+        if (pageWidth > 600) {
+            const share = document.querySelectorAll('.ss-btn-share');
+            share.forEach(box => {
+              box.remove();
+            });
+        }
+    }, 1000);
+}
