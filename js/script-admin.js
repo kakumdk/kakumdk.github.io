35150f7aa4c86013a9844b569e1e1501e2b8a3a1
karthikdksrijan <karthik.dk@srijan.net>
2024-04-09 15:45:47 +0530

    update

---
+++ js/script-admin.js
@@ -0,0 +1,31 @@
+/******************************************************************************************************************/
+/************************************** Show Ads on Content *******************************************************/
+/******************************************************************************************************************/
+$(function() {
+    showAdsOnPages();
+});
+function showAdsOnPages() {
+    var adScript = document.createElement("script");
+    if (hasElementWithId('google-ad-1')) {
+        createAd("google-ad-1", "ca-pub-2683569506254139", "5228875751", "fluid");
+    }
+    if (hasElementWithId('google-ad-2')) {
+        createAd("google-ad-2", "ca-pub-2683569506254139", "9621211280", "fluid");
+    }
+}
+function createAd(containerId, adClientId, adSlotId, adFormat) {
+    // Create a div element for the ad unit
+    var adUnit = document.createElement("ins");
+    adUnit.className = "adsbygoogle";
+    adUnit.style.display = "block";
+    adUnit.setAttribute("data-ad-client", adClientId);
+    adUnit.setAttribute("data-ad-slot", adSlotId);
+    adUnit.setAttribute("data-ad-format", adFormat);
+    // Append the ad unit to the container
+    document.getElementById(containerId).appendChild(adUnit);
+    // Initialize the ad
+    (adsbygoogle = window.adsbygoogle || []).push({});
+}
+function hasElementWithId(id) {
+    return document.getElementById(id) !== null;
+}
