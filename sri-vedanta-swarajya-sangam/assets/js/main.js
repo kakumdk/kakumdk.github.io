5705aa76839b1b26a5d8717879570fedc2f3b66e
karthikdksrijan <karthik.dk@srijan.net>
2024-08-25 17:49:16 +0530

    update

--- sri-vedanta-swarajya-sangam/assets/js/main.js
+++ sri-vedanta-swarajya-sangam/assets/js/main.js
@@ -199,3 +199,34 @@ class PDFPreviewer {
 document.addEventListener('DOMContentLoaded', () => {
   new PDFPreviewer('a.preview-pdf');
 });
+
+/******************************************************************************************************************/
+/********************************** Performance - Defer offscreen images ******************************************/
+/******************************************************************************************************************/
+document.addEventListener("DOMContentLoaded", function() {
+  const lazyImages = document.querySelectorAll('img.lazy');
+  if ('IntersectionObserver' in window) {
+      const lazyLoad = (entries, observer) => {
+          entries.forEach(entry => {
+              if (entry.isIntersecting) {
+                  const img = entry.target;
+                  img.src = img.getAttribute('data-src');
+                  img.classList.remove('lazy');
+                  observer.unobserve(img);
+              }
+          });
+      };
+      const observer = new IntersectionObserver(lazyLoad, { rootMargin: "0px 0px 50px 0px" });
+      lazyImages.forEach(image => observer.observe(image));
+  }
+  else {
+      // Fallback for browsers that do not support IntersectionObserver
+      const loadImages = () => {
+          lazyImages.forEach(image => {
+              image.src = image.getAttribute('data-src');
+              image.classList.remove('lazy');
+          });
+      };
+      window.addEventListener('load', loadImages);
+  }
+});
