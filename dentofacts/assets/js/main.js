1a6bfd95aca27b368524cc67519f249bcff5a4ba
Karthik Kumar D K <heykarthikwithu@gmail.com>
2024-10-24 12:05:04 +0530

    update

--- dentofacts/assets/js/main.js
+++ dentofacts/assets/js/main.js
@@ -251,3 +251,61 @@ document.addEventListener("DOMContentLoaded", function() {
     readingTimeDiv.textContent = `${readingTime} min${readingTime > 1 ? 's read' : 'read'}`;
   }
 });
+
+/******************************************************************************************************************/
+/************************************************** webpusher *****************************************************/
+/******************************************************************************************************************/
+(function(w, d, s, id) {
+  if (typeof(w.webpushr) !== 'undefined') return;
+  w.webpushr = w.webpushr || function() {
+      (w.webpushr.q = w.webpushr.q || []).push(arguments)
+  };
+  var js, fjs = d.getElementsByTagName(s)[0];
+  js = d.createElement(s);
+  js.id = id;
+  js.async = 1;
+  js.src = "https://cdn.webpushr.com/app.min.js";
+  fjs.parentNode.appendChild(js);
+}(window, document, 'script', 'webpushr-jssdk'));
+webpushr('setup', {
+  'key': 'BN9JHKmZWB5ivR9Sv2l_rgsouuLmGD2wsqIJmTsobxJMz4byi2Hnk1Fk50vNqX5HgfMuPD2GKjs1cUb4OpOStDM'
+});
+
+/******************************************************************************************************************/
+/************************************************** consent *****************************************************/
+/******************************************************************************************************************/
+document.addEventListener("DOMContentLoaded", function() {
+  cookiePolicy();
+});
+function cookiePolicy() {
+  var key = 'dentofacts-cookie';
+  var cookie = getStoredValue(key);
+  // console.log(cookie);
+  if (cookie === 'true') {
+      $("#policy-container").hide();
+  }
+  $(".policy-button").click(function() {
+      storeValue(key, 'true');
+      $("#policy-container").hide(1000);
+  });
+  $("#policy-container span.x").click(function() {
+      $("#policy-container").hide(1000);
+  });
+  setTimeout(function() {
+      storeValue(key, '');
+  }, 60 * 60 * 1000);
+}
+function storeValue(key, value) {
+  if (localStorage) {
+      localStorage.setItem(key, value);
+  } else {
+      $.cookies.set(key, value);
+  }
+}
+function getStoredValue(key) {
+  if (localStorage) {
+      return localStorage.getItem(key);
+  } else {
+      return $.cookies.get(key);
+  }
+}
