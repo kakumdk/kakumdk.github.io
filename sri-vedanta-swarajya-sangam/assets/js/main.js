9942e3c16fe28341011ff4105bfa0cfef944e36e
Karthik Kumar D K <heykarthikwithu@gmail.com>
2024-12-02 09:01:05 +0530

    update

--- sri-vedanta-swarajya-sangam/assets/js/main.js
+++ sri-vedanta-swarajya-sangam/assets/js/main.js
@@ -84,15 +84,15 @@
   /**
    * Animation on scroll function and init
    */
-  function aosInit() {
-    AOS.init({
-      duration: 600,
-      easing: 'ease-in-out',
-      once: true,
-      mirror: false
-    });
-  }
-  window.addEventListener('load', aosInit);
+  // function aosInit() {
+  //   AOS.init({
+  //     duration: 600,
+  //     easing: 'ease-in-out',
+  //     once: true,
+  //     mirror: false
+  //   });
+  // }
+  // window.addEventListener('load', aosInit);
 
   /**
    * Initiate glightbox
