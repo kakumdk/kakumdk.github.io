f6e904b9d9d20d0160968e185220317e6f6c1f5b
Karthik Kumar D K <heykarthikwithu@gmail.com>
2025-01-02 13:42:32 +0530

    update webpusher

--- dentofacts/assets/js/main.js
+++ dentofacts/assets/js/main.js
@@ -420,12 +420,9 @@ document.addEventListener("DOMContentLoaded", function () {
           alert("Unable to load time slots. Please try again later.");
       }
   }
-});
-document.addEventListener("DOMContentLoaded", function () {
-  // Check if the current page URL matches the desired URL
   if (window.location.pathname === "/dentofacts/online-dental-consultation.html") {
-      // Call the function only on the specified page
-      populateTimeSlots();
+    // Call the function only on the specified page
+    populateTimeSlots();
   }
 });
 document.getElementById("consultationForm").addEventListener("submit", function (event) {
